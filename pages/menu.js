0c7b559629fa4365ec200950e0f8f6899b04bf4d
Tuguldur <aa402115@gmail.com>
2022-05-08 13:10:52 +0800

    seo fix image

--- pages/menu.js
+++ pages/menu.js
@@ -1,21 +1,41 @@
 import Image from "next/image";
+import Head from "next/head";
 const Page = () => {
   return (
-    <main className="mt-12 relative flex flex-col items-center">
-      {[...new Array(5)].map((_, index) => {
-        let order = index.toString().padStart(3, "0");
-        return (
-          <div>
-            <Image
-              src={`/menu/${order}.png`}
-              width="500px"
-              height="1088px"
-              alt={`Menu Page ${order}`}
-            />
-          </div>
-        );
-      })}
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>UI Lounge & Restaurant Menu</title>
+        <meta
+          property="og:title"
+          content="UI Lounge & Restaurant Menu"
+          key="ogtitle"
+        />
+        <meta
+          property="og:description"
+          content="UI Lounge & Restaurant Menu"
+          key="ogdesc"
+        />
+        <meta property="og:image" content="/menu/001.png" />
+      </Head>
+      <main className="mt-12 relative flex flex-col items-center">
+        {[...new Array(5)].map((_, index) => {
+          let order = index.toString().padStart(3, "0");
+          return (
+            <div>
+              <Image
+                key={index}
+                src={`/menu/${order}.png`}
+                width="500px"
+                height="1088px"
+                alt={`Menu Page ${order}`}
+              />
+            </div>
+          );
+        })}
+      </main>
+    </>
   );
 };
 export default Page;
